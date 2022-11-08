24b0a734443ca5862a338212629926990e3803fe
jackclennon <59452276+jackclennon@users.noreply.github.com>
2022-11-08 20:05:14 +0000

    Update proj4.r

--- proj4.r
+++ proj4.r
@@ -1,41 +1,88 @@
-hessian<-function(theta,func,eps){
+hessian <- function(theta, grad, eps) {
+  g<-grad
   n<-length(theta)
   hess<-matrix(nrow=n,ncol=n)
+  h<-rep(0, 2)
+  h[1] <- eps
+  h[2] <- eps
   for (i in 1:n){
     for (j in 1:n){
       e_i=rep(0,n)
       e_i[i]=1
       e_j=rep(0,n)
       e_j[j]=1
-      plus_plus<-func(theta+eps*e_i + eps*e_j)
-      plus_minus<-func(theta+eps*e_i - eps*e_j)
-      minus_plus<-func(theta-eps*e_i + eps*e_j)
-      minus_minus<-func(theta-eps*e_i - eps*e_j)
-      finitedifferencing<-(plus_plus-plus_minus-minus_plus+minus_minus)/(4*eps^2)
-      hess[i,j]<-finitedifferencing
-      
+      hess[i, j] = (g(theta + h[j]*e_j)[i] - g(theta-h[j]*e_j)[i])/4*eps +  (g(theta+h[i]*e_i)[j] - g(theta-h[i]*e_i)[j])/4*eps
     }
   }
   return(hess)
 }
 
-newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6){
+newt <- function(theta,func,grad,hess,...,tol=1e-8,max.half=20,eps=1e-6) {#fscale=1,maxit=100,max.half=20){
   g <- grad(theta)
-  if (hess==NULL){
-    hess<-hessian(g,func,eps)
+  if (is.null(hess)){
+    hess<-function(theta){hessian(theta,grad,eps)}
   }
-  while (length(g[g <= tol]) > 0) {
+  
+  #check if the hessian is positive definite. If it isn't, the we perturb it
+  if (inherits(try(chol(hess), silent=TRUE), "try-error")) {
+    #perturb the hessian if it isn't positive definite
+    
+    #check that this value for the dk works
+    h0 <- hess
+    dk <- eps/sqrt(eps)
+    hess <- function(theta) {h0(theta) + diag(length(theta))*dk}
+  }
+  
+  
+  while (length(g[g > tol]) > 0) {
     #Need to find a way to get rid of the solve
-    theta <- theta - solve(hess(theta)) %*% grad(theta)
+    delta <- solve(hess(theta)) %*% grad(theta)
+    
+    #step-halving
+    #we need to check that the function's value is actually being minimised
+    counter <- 0
+    while(func(theta + delta) <= func(theta)) {
+      if (counter >= max.half) {
+        #error goes here
+        break
+      }
+      delta <- delta/2
+      counter <- counter + 1
+    }
+    
+    theta <- theta - delta
+    
     g <- grad(theta)
   }
   theta
 }
+rb <- function(th,k=2) {
+  k*(th[2]-th[1]^2)^2 + (1-th[1])^2
+}
+gb <- function(th,k=2) {
+  c(-2*(1-th[1])-k*4*th[1]*(th[2]-th[1]^2),k*2*(th[2]-th[1]^2))
+}
+hb <- function(th,k=2) {
+  h <- matrix(0,2,2)
+  h[1,1] <- 2-k*2*(2*(th[2]-th[1]^2) - 4*th[1]^2)
+  h[2,2] <- 2*k
+  h[1,2] <- h[2,1] <- -4*k*th[1]
+  h
+}
 
-#####IDEA FOR STEP HALVING:
-step size is given by the triangle on page 66
-while (func(theta + stepsize) => func(theta))
-stepsize<-stepsize/2
-if we go through max.half iterations in this while loop
-give up and print some error
+rc<-function(theta){
+  return(theta[1]^2)
+}
+
+gc<-function(theta){
+  return(c(2*theta[1],0))
+}
+hc<-function(theta){
+  h<-matrix(0,2,2)
+  h[1,1]<-2
+  return(h)
+}
+
+newt(c(1, 1), rc, gc, hc)
+
 
