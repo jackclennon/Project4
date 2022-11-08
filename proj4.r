e7b6d39884d8a320c4313944d2fb2b826732a58f
DenisZorba <113779596+DenisZorba@users.noreply.github.com>
2022-11-08 01:08:19 +0000

    Update proj4.r

--- proj4.r
+++ proj4.r
@@ -1,25 +1,33 @@
-newt <- function(theta, func, grad, hess=NULLL, ..., tol=1e-8) {
+hessian<-function(theta,func,eps){
+  n<-length(theta)
+  hess<-matrix(nrow=n,ncol=n)
+  for (i in 1:n){
+    for (j in 1:n){
+      e_i=rep(0,n)
+      e_i[i]=1
+      e_j=rep(0,n)
+      e_j[j]=1
+      plus_plus<-func(theta+eps*e_i + eps*e_j)
+      plus_minus<-func(theta+eps*e_i - eps*e_j)
+      minus_plus<-func(theta-eps*e_i + eps*e_j)
+      minus_minus<-func(theta-eps*e_i - eps*e_j)
+      finitedifferencing<-(plus_plus-plus_minus-minus_plus+minus_minus)/(4*eps^2)
+      hess[i,j]<-finitedifferencing
+      
+    }
+  }
+  return(hess)
+}
+
+newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6){
   g <- grad(theta)
+  if (hess==NULL){
+    hess<-hessian(theta,func,eps)
+  }
   while (length(g[g <= tol]) > 0) {
-    print(theta)
     #Need to find a way to get rid of the solve
     theta <- theta - solve(hess(theta)) %*% grad(theta)
     g <- grad(theta)
   }
   theta
-}
-
-#Example function, with gradient and hessian
-
-func <- function(theta) {
-  log(theta[1]) + theta[2] - theta[1]*exp(theta[2])
-}
-
-grad <- function(theta) {
-  c(1/theta[1] - exp(theta[1]), 1-theta[1]*exp(theta[2]))
-}
-
-hess <- function(theta) {
-  v <- c(-theta[1]^-2, -exp(theta[2]), -exp(theta[2]), -theta[1]*exp(theta[2]))
-  matrix(v, 2, 2)
 }
